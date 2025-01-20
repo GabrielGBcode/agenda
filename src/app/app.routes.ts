f6223522063bca972a9b657169d8be1640747528
GabrielGBcode <gabrielgb.xyz@gmail.com>
2025-01-20 11:41:57 -0300

    Created login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: LoginScreenComponent }];
