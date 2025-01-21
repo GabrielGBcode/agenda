98aaaad488645663b2972362db5b42a3380eaa78
GabrielGBcode <gabrielgb.xyz@gmail.com>
2025-01-21 11:27:09 -0300

    update src

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = ''; // URL da API
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: { email: string; password: string }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+
+  logout(): void {
+    localStorage.removeItem('accessToken');
+  }
+
+  setToken(token: string): void {
+    localStorage.setItem('accessToken', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('accessToken');
+  }
+}
